6db748c6e5cefb83afaf5e6734e8c7541e4f4f06
Aron Hoekstra <58637612+nullvalue0@users.noreply.github.com>
2026-03-28 17:03:28 -0500

    3/28 updates
    swapped crystals for TH parts

--- delete_traces.py
+++
@@ -1,8 +0,0 @@
-import pcbnew
-b = pcbnew.GetBoard()
-tracks = b.Tracks()
-for t in tracks:
-    if t.GetWidth() == pcbnew.FromMM(0.20):
-        t.SetSelected()
-        
-pcbnew.Refresh()
--- delete_vias.py
+++
@@ -1,6 +0,0 @@
-import pcbnew
-for v in pcbnew.GetBoard().GetTracks():
-    if type(v) is pcbnew.PCB_VIA and v.GetWidth() == pcbnew.FromMM(0.6):
-        v.SetSelected()
-        
-pcbnew.Refresh()
